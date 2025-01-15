39e5d14ddc95a098b613672dc764d24ba284edbe
Masterchef365 <duncan.freeman1@gmail.com>
2025-01-14 23:31:48 -0800

    Time complexity test

---
+++ examples/time_complexity.rs
@@ -0,0 +1,32 @@
+use std::time::Instant;
+
+use rand::Rng;
+use rect_intersect::{brute_force_intersect, intersect, random_rects, to_comparable};
+
+fn main() {
+    let mut rng = rand::thread_rng();
+
+    println!("size,brute,algo");
+
+    for i in 0..15 {
+        let n = 2_usize.pow(i);
+
+        let rects = random_rects(n, rng.gen());
+
+        let algo_time = Instant::now();
+        let algo = intersect(&rects);
+        let algo_time = algo_time.elapsed().as_secs_f32();
+
+        let brute_time = Instant::now();
+        let brute = brute_force_intersect(&rects);
+        let brute_time = brute_time.elapsed().as_secs_f32();
+
+        println!("{},{},{}", n, brute_time, algo_time);
+
+        assert_eq!(
+            to_comparable(algo),
+            to_comparable(brute),
+        );
+
+    }
+}
