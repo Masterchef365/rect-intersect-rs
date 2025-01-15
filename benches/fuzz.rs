7130059e5ee905c23465b38c5f3a6a264dcca4ec
Masterchef365 <duncan.freeman1@gmail.com>
2025-01-15 02:16:30 -0800

    Smart benchmark

--- benches/fuzz.rs
+++ benches/fuzz.rs
@@ -1,13 +1,18 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rect_intersect::{intersect, random_rects_detailed};
+use rect_intersect::{brute_force_intersect, intersect, random_rects_detailed};
 
-fn test(size: i32) {
-    let rects = random_rects_detailed(size as _, size as _, size as _, 50);
-    black_box(intersect(&rects));
-}
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| test(black_box(100))));
+    for i in (10..90).step_by(20) {
+        let rects = random_rects_detailed((i*i) as _, i as _, (i * 15) as _, 50);
+        c.bench_function(&format!("random {}", i*i), |b| b.iter(|| {
+            black_box(intersect(black_box(&rects)));
+        }));
+        c.bench_function(&format!("brute {}", i*i), |b| b.iter(|| {
+            black_box(brute_force_intersect(black_box(&rects)));
+        }));
+
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
