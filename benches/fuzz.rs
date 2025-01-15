d6a387825d6667c5fdf1175f5a29b81499e5549f
Masterchef365 <duncan.freeman1@gmail.com>
2025-01-15 13:49:37 -0800

    Bench params

--- benches/fuzz.rs
+++ benches/fuzz.rs
@@ -3,14 +3,18 @@ use rect_intersect::{brute_force_intersect, intersect, random_rects_detailed};
 
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    for i in (10..90).step_by(20) {
+    for i in (30..130).step_by(20) {
         let rects = random_rects_detailed((i*i) as _, i as _, (i * 15) as _, 50);
+
         c.bench_function(&format!("random {}", i*i), |b| b.iter(|| {
             black_box(intersect(black_box(&rects)));
         }));
-        c.bench_function(&format!("brute {}", i*i), |b| b.iter(|| {
-            black_box(brute_force_intersect(black_box(&rects)));
-        }));
+
+        if i*i < 10_000 {
+            c.bench_function(&format!("brute/random {}", i*i), |b| b.iter(|| {
+                black_box(brute_force_intersect(black_box(&rects)));
+            }));
+        }
 
     }
 }
